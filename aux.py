0dfd4b861e7ad14e006e725da816b8a866b54979
saick123 <matheussaick@gmail.com>
2025-02-14 14:55:43 -0300

    fazer o sortei agora de funções de destruição e de construção

--- aux.py
+++ aux.py
@@ -4,6 +4,7 @@ import math
 import random
 import time
 import copy
+import numpy as np
 
 folders = ['Vrp-Set-' + c for c in ['A', 'B', 'F']]
 files = []
@@ -71,8 +72,11 @@ if False:
     print(row)
 
 
-
-def initial_solution_generator(instance : CVRPInstance):
+def get_distance(node1 : int , node2 : int):
+    dist = instance.distance_matrix
+    return int(dist[node1][node2])
+    
+def initial_solution_generator(instance : CVRPInstance) -> list[list[int]]:
 
     routes = []
     clients = []
@@ -80,7 +84,7 @@ def initial_solution_generator(instance : CVRPInstance):
 
     while True: 
         routes = []
-        clients = [c for c in range(instance.number_nodes) if c != instance.depot_idx]
+        clients = copy.deepcopy(instance.clients)
     
         for _ in range(instance.number_trucks):
             truck_load = 0
@@ -103,13 +107,15 @@ def initial_solution_generator(instance : CVRPInstance):
         
     return routes
 
+###### mudei 
 def get_load_from_route(route):
+    demands = instance.demands
     total = 0
     for node_idx in route:
-        total += instance.demands[node_idx]
+        total += demands[node_idx]
     return total
 
-            
+### lixo       
 def check_all_problems():
     i = 1
     for name, problems in files:
@@ -122,21 +128,17 @@ def check_all_problems():
                 exit(1)
             i+=1    
                 
-#check_all_problems()
-##instance = CVRPInstance(path=teste_path)
-##init_sol = initial_solution_generator(instance)
-##print(init_sol)
 
 def evaluation_function(solution: list[list[int]]):
-   
+    dist = instance.distance_matrix
     total_cost = 0
     for route in solution:
         for i in range(len(route) - 1):
-            total_cost += instance.distance_matrix[route[i]][route[i+1]]
-        total_cost += instance.distance_matrix[instance.depot_idx][route[0]]
-        total_cost += instance.distance_matrix[instance.depot_idx][route[-1]]
+            total_cost += dist[route[i]][route[i+1]]
+        total_cost += dist[instance.depot_idx][route[0]]
+        total_cost += dist[instance.depot_idx][route[-1]]
         
-    return total_cost
+    return int(total_cost)
 
 
 def random_removal(solution: list[list[int]], q: int):
@@ -153,6 +155,38 @@ def random_removal(solution: list[list[int]], q: int):
         D.append(solution[r_idx].pop(c_idx))
         q_ -=1
 
+    return solution, D
+
+def removal_cost(route, client, client_idx):
+    dist = instance.distance_matrix
+    succ = instance.depot_idx if client_idx == len(route) - 1 else route[client_idx + 1]
+    pred = instance.depot_idx if client_idx == 0 else route[client_idx - 1]
+    return dist[pred][client] + dist[client][succ] - dist[pred][succ]
+
+def worst_removal(solution: list[list[int]], q: int):
+    
+    r_costs = []
+    D = []
+    
+    for route_idx, route in enumerate(solution):
+        for c_idx, c in enumerate(route):
+            r_cost = removal_cost(route, c, c_idx)
+            r_costs.append([c, route_idx, r_cost])
+
+    r_costs.sort(key=lambda x: x[-1], reverse=True)
+    q_ = q
+    p = 3 # verificar quais são valores de p possíveis aqui
+    
+    while q_:
+        y = random.random()
+        idx = math.floor(len(r_costs)*(math.pow(y, p)))
+        
+        client, route_idx, r_cost = r_costs.pop(idx)
+        
+        D.append(client)
+        solution[route_idx].remove(client)
+        q_ -=1
+    
     return solution, D
 
 
@@ -219,11 +253,11 @@ def verify_solution(solution: list[list[int]]):
     return True
 
 def generate_new_solution(solution: list[list[int]]):
-   
+    q = int(instance.number_nodes*0.1)
     candidate_solution = None
-   
     while True:    
-        parcial, unassigned = random_removal(copy.deepcopy(solution), int(instance.number_nodes*0.15))
+        #parcial, unassigned = random_removal(copy.deepcopy(solution), q)
+        parcial, unassigned = worst_removal(copy.deepcopy(solution), q)
         candidate_solution = greedy_repair(parcial, unassigned)
         if candidate_solution is None:
             continue 
@@ -233,7 +267,7 @@ def generate_new_solution(solution: list[list[int]]):
 ############ fazer um método de destruir e outro de reparar pra testar
 class ALNS:
     
-    def __init__(self, feasible_solution: list[list[int]], evaluation_foo):
+    def __init__(self, feasible_solution: list[list[int]], evaluation_foo, destroy_methods: list = [], repair_methos: list = []):
         
         self.evaluation_function = evaluation_foo
         self.p_iminus = []
@@ -243,43 +277,72 @@ class ALNS:
         self.initial_solution = copy.deepcopy(feasible_solution)
         self.best_solution = copy.deepcopy(feasible_solution)
         self.best_cost = self.evaluation_function(self.best_solution) 
-        
-    def run(self, seconds_limit: int = 300, alpha: int = 0.999, intial_temp: int = 100):
     
-        x_solution = [x for x in self.best_solution]
+    ################## 
+    ### as fuções vão receber a solução(cópia) e os hiperparâmetros que é um dicionáro dado pelo usuário
+    def __gen_new_sol(self, solution):
+        pass
+        
+    def run(self, seconds_limit: int = 300, max_iteration = np.inf, alpha: int = 0.999, initial_temp: int = 100, verbose = False):
+    
+        x_solution = copy.deepcopy(self.initial_solution)
         x_cost = self.evaluation_function(x_solution)
+        
+        current_iter = 1 ##### começar na iteração 1
+        current_temp = initial_temp
+        min_temp = 1e-6
+        costs = []
         
         start_time = time.time()
         while True:
             
             current_time = time.time()
-            if(current_time - start_time) >= 300:
+            
+            if(current_time - start_time) >= seconds_limit or current_iter > max_iteration:
                 break
             
+            xt_solution = generate_new_solution(x_solution)
+            xt_cost = evaluation_function(xt_solution)
             
-            
+            flag_new_best = False ### usado para não fazer cópias desnecessárias
+            if xt_cost < self.best_cost:
                 
-        
+                flag_new_best = True
+                x_solution, x_cost = copy.deepcopy(xt_solution), xt_cost
+                self.best_solution, self.best_cost = copy.deepcopy(xt_solution), xt_cost
+                costs.append(self.best_cost)
+            
+            if current_temp > min_temp:
+                
+                if (random.random() < math.exp( (x_cost - xt_cost) / current_temp)) and not flag_new_best:
+                    x_solution, x_cost = copy.deepcopy(xt_solution), xt_cost
+                    
+                current_temp = initial_temp / math.log(1 + current_iter)
+                #current_temp = initial_temp / (current_iter +  1)
+                #current_temp = current_temp * alpha    
+            
+            if current_iter % 1000 == 0 and verbose: ## desativei rapidinho
+                print(f'Iteration {current_iter}, Temperature {current_temp:.3f}, Best evaluation {self.best_cost:.5f}, Time {current_time - start_time}')
+            
+            
+            current_iter +=1
+            
+
+
+def run_all():
+    
+    global instance, files
+
+    for case_name, filepaths in files:
+        for vrp_path, sol_path in filepaths:
+            instance = CVRPInstance(path=vrp_path)
+            init_solution = initial_solution_generator(instance)
+            alns = ALNS(init_solution, evaluation_function)
+            alns.run(initial_temp=10000, max_iteration=10000, verbose=False)
+            print(f'FOLDER {case_name}, VRP_PATH = {vrp_path}, Best eval {alns.best_cost}, Well-Know Best cost {instance.optimal_value}, Feasible? {verify_solution(alns.best_solution)}')
 
 
 if __name__ == '__main__':
 
-    teste_path = '/home/matheusmartin/Área de Trabalho/T2-Otimizacao-Metaheuristicas-CVRP-com-ALNS/Vrp-Set-A/A/A-n32-k5.vrp'
-   
-    instance = CVRPInstance(path=teste_path)
-    init_sol = initial_solution_generator(instance)
-    while True:
+    run_all()
         
-        parcial, unassigned = random_removal(copy.deepcopy(init_sol), int(instance.number_nodes*0.2))
-        candidate_solution = greedy_repair(parcial, unassigned)
-        
-        if candidate_solution is None:
-            continue
-                        
-        print(evaluation_function(init_sol), evaluation_function(candidate_solution))
-        
-        break
-
-    
-    
-    
