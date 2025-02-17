2864b7ab903278b8668f7e35f2a13a29469cfce7
saick123 <matheussaick@gmail.com>
2025-02-17 08:18:14 -0300

    resutlados gerados, criar tabelas latex e colocar no artigo e começar a escrever

--- results_to_csv.py
+++ results_to_csv.py
@@ -1,15 +1,12 @@
 import os
 
-
 ## instance, param1(q_max), param2(r), f_sol_optimal, f_mh_min, f_mh_mean, temp_min, temp_mean, gap_min, gap_mean
-"A-n32-k5, 0.2, 0.1, 784, 788, 788.0, 4.190832853317261, 4.190832853317261, 0.00510204081632653, 0.00510204081632653"
-
-
+#"A-n32-k5, 0.2, 0.1, 784, 788, 788.0, 4.190832853317261, 4.190832853317261, 0.00510204081632653, 0.00510204081632653"
 
 if __name__ == "__main__":
     
-    folder_path = 'args_results'
-    save_name = 'hp_selection.csv'
+    folder_path = 'final_results'
+    save_name = 'final_results.csv'
     header = 'instance; param1; param2; f_sol_optimal; f_mh_min; f_mh_mean; temp_min; temp_mean; gap_min; gap_mean'
     
     files = os.listdir(folder_path)
@@ -28,9 +25,13 @@ if __name__ == "__main__":
             line[i] = float(line[i])
         for i in range(len(line)):
             if i >=6:
-                line[i] = f'{line[i]:.7}'
+                #line[i] = f'{line[i]:.7}'
+                
+                line[i] = f'{line[i]:.7}'.replace('.', ',')
             else:
-                line[i] = str(line[i])
+                #line[i] = str(line[i])
+                
+                line[i] = str(line[i]).replace('.', ',') 
     
     with open(save_name, 'w') as out_file:
         print(header, file=out_file)
