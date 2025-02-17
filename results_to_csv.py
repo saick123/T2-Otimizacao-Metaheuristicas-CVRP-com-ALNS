104645c028fedd28df754129c17b899526840d7c
saick123 <matheussaick@gmail.com>
2025-02-17 13:45:22 -0300

    nao sei

--- results_to_csv.py
+++ results_to_csv.py
@@ -3,11 +3,17 @@ import os
 ## instance, param1(q_max), param2(r), f_sol_optimal, f_mh_min, f_mh_mean, temp_min, temp_mean, gap_min, gap_mean
 #"A-n32-k5, 0.2, 0.1, 784, 788, 788.0, 4.190832853317261, 4.190832853317261, 0.00510204081632653, 0.00510204081632653"
 
+colunas_args_results = {0, 1, 2, 4}
+colunas_final_results = {0, 3, 4, 5, 6, 7, 8, 9}
+
+
 if __name__ == "__main__":
     
     folder_path = 'final_results'
     save_name = 'final_results.csv'
     header = 'instance; param1; param2; f_sol_optimal; f_mh_min; f_mh_mean; temp_min; temp_mean; gap_min; gap_mean'
+    colunas_filtro = colunas_final_results
+    header = header.split(';')
     
     files = os.listdir(folder_path)
     
@@ -33,8 +39,13 @@ if __name__ == "__main__":
                 
                 line[i] = str(line[i]).replace('.', ',') 
     
+    lines.insert(0, header) ## colocando header no começo
+    
+    for i, line in enumerate(lines):
+        line = [x for idx, x in enumerate(line) if idx in colunas_filtro]
+        lines[i] = line
+    
     with open(save_name, 'w') as out_file:
-        print(header, file=out_file)
         for line in lines:
             str_ = ';'.join(line)
             print(str_, file=out_file)
